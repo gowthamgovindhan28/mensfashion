2d28fed9ca4abd87f87520d13d64bff0c5c1ac6b
Gowtham G <148966474+gowthamgovindhan28@users.noreply.github.com>
2024-04-22 23:29:16 +0530

    Add files via upload

---
+++ src/com/gowtham/service/DemandService.java
@@ -0,0 +1,17 @@
+package com.gowtham.service;
+
+import java.util.List;
+
+import com.gowtham.beans.DemandBean;
+
+public interface DemandService {
+
+	public boolean addProduct(String userId, String prodId, int demandQty);
+
+	public boolean addProduct(DemandBean userDemandBean);
+
+	public boolean removeProduct(String userId, String prodId);
+
+	public List<DemandBean> haveDemanded(String prodId);
+
+}
---
+++ src/com/gowtham/service/TransService.java
@@ -0,0 +1,6 @@
+package com.gowtham.service;
+
+public interface TransService {
+
+	public String getUserId(String transId);
+}
